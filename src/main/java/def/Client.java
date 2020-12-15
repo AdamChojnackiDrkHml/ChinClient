e1a868f757168ebcecef3385b5c5106c5efb2998
Adam Chojnacki <adam.chojnacki.syl@gmail.com>
2020-12-15 14:39:20 +0100

    Dodałem klasę row i eksperymenty z rysowaniem

--- src/main/java/def/Client.java
+++ src/main/java/def/Client.java
@@ -1,20 +1,14 @@
 package def;
 
-import java.awt.Font;
-import java.awt.Color;
-import java.awt.GridLayout;
-import java.awt.GridBagLayout;
-import java.awt.BorderLayout;
+import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.Scanner;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.concurrent.Flow;
 
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 /**
  * A client for a multi-player tic tac toe game. Loosely based on an example in
@@ -32,8 +26,8 @@ public class Client {
     private JFrame frame = new JFrame("Tic Tac Toe");
     private JLabel messageLabel = new JLabel("...");
 
-    private Square[] board = new Square[9];
     private Square currentSquare;
+    private Row[] rows = new Row[17];
 
     private Socket socket;
     private Scanner in;
@@ -50,17 +44,41 @@ public class Client {
 
         JPanel boardPanel = new JPanel();
         boardPanel.setBackground(Color.black);
-        boardPanel.setLayout(new GridLayout(3, 3, 2, 2));
-        for (int i = 0; i < board.length; i++) {
-            final int j = i;
-            board[i] = new Square();
-            board[i].addMouseListener(new MouseAdapter() {
-                public void mousePressed(MouseEvent e) {
-                    currentSquare = board[j];
-                    out.println("MOVE " + j);
+        boardPanel.setLayout(new GridLayout(17, 1, 1, 1));
+        for (int i = 0; i < rows.length; i++)
+        {
+            rows[i] = new Row(i);
+            rows[i].setSize(boardPanel.getSize());
+
+            for(int j = 0; j < rows.length; j++)
+            {
+
+                final int k = i;
+                final int l = j;
+                Square square = new Square();
+
+                if(isThisValidPool(j,i))
+                {
+                    square.setBackground(Color.MAGENTA);
                 }
-            });
-            boardPanel.add(board[i]);
+                square.addMouseListener(new MouseAdapter()
+                {
+                    public void mousePressed(MouseEvent e)
+                    {
+                        try
+                        {
+                            currentSquare = (Square) rows[k].getContent(l);
+                        } catch (Exception exception)
+                        {
+                            exception.printStackTrace();
+                        }
+                        out.println("MOVE (" + k + "," + l + ")");
+                    }
+                });
+                rows[i].setContent(j, square);
+            }
+            rows[i].initializePools();
+            boardPanel.add(rows[i]);
         }
         frame.getContentPane().add(boardPanel, BorderLayout.CENTER);
     }
@@ -75,7 +93,7 @@ public class Client {
      * message.
      */
     public void play() throws Exception {
-        try {
+  /*      try {
             String response = in.nextLine();
             char mark = response.charAt(8);
             char opponentMark = mark == 'X' ? 'O' : 'X';
@@ -88,8 +106,8 @@ public class Client {
                     currentSquare.repaint();
                 } else if (response.startsWith("OPPONENT_MOVED")) {
                     int loc = Integer.parseInt(response.substring(15));
-                    board[loc].setText(opponentMark);
-                    board[loc].repaint();
+               //     board[loc][5].setText(opponentMark);
+                 //   board[loc][5].repaint();
                     messageLabel.setText("Opponent moved, your turn");
                 } else if (response.startsWith("MESSAGE")) {
                     messageLabel.setText(response.substring(8));
@@ -113,7 +131,7 @@ public class Client {
         } finally {
             socket.close();
             frame.dispose();
-        }
+        } */
     }
 
     static class Square extends JPanel {
@@ -131,7 +149,6 @@ public class Client {
             label.setText(text + "");
         }
     }
-
     public static void main(String[] args) throws Exception {
         if (args.length != 1) {
             System.err.println("Pass the server IP as the sole command line argument");
@@ -143,5 +160,18 @@ public class Client {
         client.frame.setVisible(true);
         client.frame.setResizable(false);
         client.play();
+    }
+    private static boolean isThisValidPool(int xCord, int yCord)
+    {
+        return !(((yCord == 13 || yCord == 3) && (xCord > 9 || xCord < 6)) ||
+                ((yCord == 14 || yCord == 2) && (xCord > 9 || xCord < 7)) ||
+                ((yCord == 15 || yCord == 1) && (xCord > 8 || xCord < 7)) ||
+                ((yCord == 16 || yCord == 0) && (xCord != 8)) ||
+                (xCord < 2) ||
+                (xCord > 14) ||
+                (xCord == 2 && (yCord > 5 && yCord < 11 )) ||
+                (xCord == 3 && yCord == 8) ||
+                (xCord == 14 && yCord > 4 && yCord < 12) ||
+                (xCord == 13 && yCord > 7 && yCord < 10));
     }
 }
